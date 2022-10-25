d35b5d936b808835be5cf993dcc13080acfc8ac6
Vinicius Leite Mello <50744076+VLMello@users.noreply.github.com>
2022-10-25 14:45:40 -0300

    Update TrafficController.java

--- Trabalho2/ex3/TrafficController.java
+++ Trabalho2/ex3/TrafficController.java
@@ -1,8 +1,25 @@
-public class TrafficController {
+import java.util.concurrent.*;
 
-    public void enterLeft() {}
-    public void enterRight() {}
-    public void leaveLeft() {}
-    public void leaveRight() {}
+public static class TrafficController {
+
+    Semaphore sem;
+
+    public TrafficController() 
+    {
+        sem = new Semaphore(1);
+    }
+
+    public void enterLeft() {
+        sem.acquire();
+    }
+    public void enterRight() {
+        sem.acquire();
+    }
+    public void leaveLeft() {
+        sem.release();
+    }
+    public void leaveRight() {
+        sem.release();
+    }
 
 }
